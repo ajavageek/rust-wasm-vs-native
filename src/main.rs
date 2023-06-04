fec228b6dea5ab29d744c41f23af68a3e1d38a33
Nicolas Fränkel <nicolas@frankel.ch>
2023-06-04 22:09:54 +0200

    Add parameters to the command-line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        println!("Hello, world!");
+    } else {
+        println!("Hello, {}!", args[1]);
+    }
 }
